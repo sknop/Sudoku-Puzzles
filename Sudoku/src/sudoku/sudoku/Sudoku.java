cffd930cbc780ac2e20b48a93292ce742d41e770
Sven Erik Knop <sknop@perforce.com>
2014-06-14 12:44:16 +0100

    Refactoring - Sudoku now has its own package sudoku.sudoku

--- Sudoku/src/sudoku/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/sudoku/Sudoku.java
@@ -1,10 +1,12 @@
-package sudoku;
+package sudoku.sudoku;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import sudoku.Cell;
+import sudoku.Point;
 import sudoku.exceptions.AddCellException;
 import sudoku.unit.Nonet;
 
