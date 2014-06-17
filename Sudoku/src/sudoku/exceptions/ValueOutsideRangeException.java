3c0294b7909a0851c8e22b3921677f04ffff0bd0
Sven Erik Knop <sknop@perforce.com>
2014-06-17 20:28:43 +0100

    Added missing serialId

--- Sudoku/src/sudoku/exceptions/ValueOutsideRangeException.java
+++ Sudoku/src/sudoku/exceptions/ValueOutsideRangeException.java
@@ -2,6 +2,7 @@ package sudoku.exceptions;
 
 public class ValueOutsideRangeException extends CellContentException
 {
+	private static final long serialVersionUID = -445994557148385322L;
 
 	public ValueOutsideRangeException(String message) {
 		super(message);
