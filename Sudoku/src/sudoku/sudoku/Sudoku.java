548c867416415c488c29afb6ec9c7c85297717a4
Sven Erik Knop <sknop@perforce.com>
2014-07-07 10:20:45 +0100

    Removed superfluous import

--- Sudoku/src/sudoku/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/sudoku/Sudoku.java
@@ -35,7 +35,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import sudoku.Cell;
