342f65fd399b08246975f302aa770d1e2c8a101b
Sven Erik Knop <sknop@perforce.com>
2014-07-09 19:52:58 +0100

    Removed superfluous import

--- Sudoku/src/sudoku/samurai/Samurai.java
+++ Sudoku/src/sudoku/samurai/Samurai.java
@@ -37,7 +37,6 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 import sudoku.Cell;
