f6382ab9c10eac740444fb7e5dfd2649a903ba1c
Sven Erik Knop <seknop@gmail.com>
2024-09-24 09:44:07 +0000

    Updated tests

--- src/test/java/sudoku/kakuro/PartitionsTest.java
+++ src/test/java/sudoku/kakuro/PartitionsTest.java
@@ -47,5 +47,10 @@ class PartitionsTest {
 
             System.out.println(STR."\{key} : \{entry}");
         }
+
+        assertEquals(p.get(new Clue(3,2)).size(), 1);
+        assertEquals(p.get(new Clue(5,2)).size(), 2);
+        assertEquals(p.get(new Clue(17,2)).size(), 1);
+        assertEquals(p.get(new Clue(45,9)).size(), 1);
     }
 }
